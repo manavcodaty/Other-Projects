d7e45dc53e0b11c33830ec6c03f6d30c28bb8dce
Manav Codaty <111678991+manavcodaty@users.noreply.github.com>
2024-02-21 05:12:54 +0000

    Refactor booking class and remove unused code

---
+++ cinema.py
@@ -0,0 +1,27 @@
+class booking():
+    def __init__(self, name, date, time, duration, location, price):
+        self.name = name
+        self.date = date
+        self.time = time
+        self.duration = duration
+        self.location = location
+        self.price = price
+    def get_booking(self):
+        self.name = input("Enter your name: ")
+        self.date = input("Enter the date (YYYY:MM:DD): ")
+        self.time = input("Enter the time (HH:MM): ")
+        self.duration = input("Enter the duration (in hours): ")
+        self.location = input("Enter the location: ")
+        self.price = input("Enter the price:")
+    def print_booking(self):
+        print(self.name)
+        print(self.date)
+        print(self.time)
+        print(self.duration)
+        print(self.location)
+        print(self.price)
+    
+    
+def ask_for_booking():
+    booking = booking.get_booking()
+    
